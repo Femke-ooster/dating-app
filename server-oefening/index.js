8e274f2207fddaff5adedf7454d8f74012abbde6
Mona Lisa <femkedrawings@gmail.com>
2020-02-28 09:27:57 +0100

    pug
    pug geinstalleerd en verschillende codes toegevoegd aan de index.js

--- server-oefening/index.js
+++ server-oefening/index.js
@@ -2,7 +2,20 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+/*geef toegang tot statische map*/
 app.use("/static", express.static(__dirname + "/static")); /**/
+
+/*Zeg tegen Express dat we de pug template engine gebruiken*/
+app.set("view engine", "pug");
+
+/*Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn*/
+app.set("views", "views");
+
+app.get("/index", (req, res) => {
+  /*method .render beschikbaar gesteld door pug op het response object*/
+  res.render("index.pug", {name: "hey", title: "bye"});
+});
+
 
 app.get("/", (req, res) => {
   res.write("<p>Hello!<p>");
@@ -14,4 +27,4 @@ app.get("/about", (req, res) => {
   res.end();
 });
 
-app.listen(port, () => console.log("Running my NodeJS server at"));
+app.listen(port, () => console.log("Running my NodeJS server at" + port));
