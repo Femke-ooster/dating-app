384e7d12033c804d5259c5758489229090509da3
Mona Lisa <femkedrawings@gmail.com>
2020-03-02 16:38:18 +0100

    week 4 backend
    ejs toegevoegd

--- server-oefening/index.js
+++ server-oefening/index.js
@@ -2,19 +2,36 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+/*geef toegang tot statische map
+app.use("/static", express.static(__dirname + "/static"));
+
+Zeg tegen Express dat we de pug template engine gebruiken
+app.set("view engine", "pug");
+
+Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn
+app.set("views", "views");
+
+app.get("/index", (req, res) => {
+  method .render beschikbaar gesteld door pug op het response object
+  res.render("index.pug", {name: "hey", title: "bye"});
+});*/
+
+
+
 /*geef toegang tot statische map*/
 app.use("/static", express.static(__dirname + "/static")); /**/
 
-/*Zeg tegen Express dat we de pug template engine gebruiken*/
-app.set("view engine", "pug");
+/*Zeg tegen Express dat we de ejs template engine gebruiken*/
+app.set("view engine", "ejs");
 
 /*Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn*/
 app.set("views", "views");
 
-app.get("/index", (req, res) => {
+app.get("/index", function(req, res){
   /*method .render beschikbaar gesteld door pug op het response object*/
-  res.render("index.pug", {name: "hey", title: "bye"});
+  res.render("index.ejs", {title: "hey", message: "bye"});
 });
+
 
 
 app.get("/", (req, res) => {
