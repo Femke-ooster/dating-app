f8fb6bbec124ad66f1fd1628337d1560dac62a55
Mona Lisa <femkedrawings@gmail.com>
2020-03-06 08:32:02 +0100

    Week 4
    Alles van week 4 toegepast:
    (behalve van het project)
    
    - slug install
    - body-parser install
    - opdracht met Express over movies toevoegen (nog niet af)
    - notities toegevoegd

--- server-oefening/index.js
+++ server-oefening/index.js
@@ -1,47 +1,43 @@
 const express = require('express');
+var slug = require('slug')
+var bodyParser = require('body-parser')
 const app = express();
 const port = 8000;
-
-/*geef toegang tot statische map
-app.use("/static", express.static(__dirname + "/static"));
-
-Zeg tegen Express dat we de pug template engine gebruiken
-app.set("view engine", "pug");
-
-Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn
-app.set("views", "views");
-
-app.get("/index", (req, res) => {
-  method .render beschikbaar gesteld door pug op het response object
-  res.render("index.pug", {name: "hey", title: "bye"});
-});*/
-
 
 
 /*geef toegang tot statische map*/
 app.use("/static", express.static(__dirname + "/static")); /**/
 
+
 /*Zeg tegen Express dat we de ejs template engine gebruiken*/
 app.set("view engine", "ejs");
+
 
 /*Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn*/
 app.set("views", "views");
 
+
 app.get("/index", function(req, res){
-  /*method .render beschikbaar gesteld door pug op het response object*/
+  /*method .render beschikbaar gesteld door ejs op het response object*/
   res.render("index.ejs", {title: "hey", message: "bye"});
 });
 
 
 
-app.get("/", (req, res) => {
-  res.write("<p>Hello!<p>");
-  res.end();
+
+app.get("/", function(req, res){
+  /*method .render beschikbaar gesteld door ejs op het response object*/
+  res.render("add.ejs");
 });
 
-app.get("/about", (req, res) => {
-  res.write("<p>Over mij<p>");
-  res.end();
-});
+
+
+app.post('/add-movie', function(req, res){
+  console.log(req.body)
+  res.end()
+})
+
+
+
 
 app.listen(port, () => console.log("Running my NodeJS server at" + port));
