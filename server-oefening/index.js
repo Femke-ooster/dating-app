208b4b66d1c793f4393a72be0459e41cd9052e78
Mona Lisa <femkedrawings@gmail.com>
2020-03-09 20:55:05 +0100

    Feature staat op Localhost:8000
    Nieuwe map aangemaakt voor de uiteindelijke feature.
    De html, css en index.js werken nu met de feature en dus kan je door de navigatie heen klikken

--- server-oefening/index.js
+++ server-oefening/index.js
@@ -1,21 +1,38 @@
 const express = require('express');
-var slug = require('slug')
-var bodyParser = require('body-parser')
+var slug = require('slug');
+var bodyParser = require('body-parser');
+var mongo = require('mongodb');
+/*env beschikbaarvoor node.js*/
+require('dotenv').config();
 const app = express();
 const port = 8000;
+
+let db;
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@cluster0-lrmlb.azure.mongodb.net/test?retryWrites=true&w=majority";
+/*const uri = "mongodb+srv://femkeooster:<password>@cluster0-lrmlb.azure.mongodb.net/test?retryWrites=true&w=majority";*/
+
+/*verbind met mongodb*/
+MongoClient.connect(uri, function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(process.env.DB_NAME)
+  db.collection('test').insertOne({
+    title: 'Hello!'
+  })
+})
+
 
 
 /*geef toegang tot statische map*/
 app.use("/static", express.static(__dirname + "/static")); /**/
 
-
 /*Zeg tegen Express dat we de ejs template engine gebruiken*/
 app.set("view engine", "ejs");
 
-
 /*Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn*/
 app.set("views", "views");
-
 
 app.get("/index", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
@@ -30,14 +47,9 @@ app.get("/", function(req, res){
   res.render("add.ejs");
 });
 
-
-
 app.post('/add-movie', function(req, res){
   console.log(req.body)
   res.end()
 })
-
-
-
 
 app.listen(port, () => console.log("Running my NodeJS server at" + port));
