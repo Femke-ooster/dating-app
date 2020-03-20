17c09866bb7a7ce019e46d400cac314aad70aa31
Mona Lisa <femkedrawings@gmail.com>
2020-03-20 19:41:28 +0100

    Onnodige bestanden verwijderd

--- statische-website/featurePE/static/JS/java.js
+++ statische-website/featurePE/static/JS/java.js
@@ -16,31 +16,31 @@ function show() {
 zoekbutton.addEventListener("click", show);
 */
 
-console.log('hellow :)');
-
-// dit zijn de notificatie div'jes
-let boost = document.getElementById('noteBoost');
-let likes = document.getElementById('noteLikes');
-let messages = document.getElementById('noteMessages');
-
-// dit zijn de nummers in p
-let valueBoost = document.getElementById('valueBoost');
-let valueLikes = document.getElementById('valueLikes');
-let valueMessages = document.getElementById('valueMessages');
-
-let type = [boost, likes, messages];
-let value = [valueBoost, valueLikes, valueMessages];
-
-function notifyBoost {
-	if(valueBoost > 0){
-		// https://stackoverflow.com/questions/9456289/how-to-make-a-div-visible-and-invisible-with-javascript
-		boost.style.visibility = 'visible';
-		// https://www.w3schools.com/js/js_htmldom_html.asp
-		document.getElementById("valueBoost").innerHTML = "1";
-	} else {
-		boost.style.visibility = 'hidden'; // hide, but lets the element keep its size
-	}
-}
+// console.log('hellow :)');
+//
+// // dit zijn de notificatie div'jes
+// let boost = document.getElementById('noteBoost');
+// let likes = document.getElementById('noteLikes');
+// let messages = document.getElementById('noteMessages');
+//
+// // dit zijn de nummers in p
+// let valueBoost = document.getElementById('valueBoost');
+// let valueLikes = document.getElementById('valueLikes');
+// let valueMessages = document.getElementById('valueMessages');
+//
+// let type = [boost, likes, messages];
+// let value = [valueBoost, valueLikes, valueMessages];
+//
+// function notifyBoost {
+// 	if(valueBoost > 0){
+// 		// https://stackoverflow.com/questions/9456289/how-to-make-a-div-visible-and-invisible-with-javascript
+// 		boost.style.visibility = 'visible';
+// 		// https://www.w3schools.com/js/js_htmldom_html.asp
+// 		document.getElementById("valueBoost").innerHTML = "1";
+// 	} else {
+// 		boost.style.visibility = 'hidden'; // hide, but lets the element keep its size
+// 	}
+// }
 
 // var boost = document.querySelector("#boost");
 // var boostActive = document.querySelector("#boostActive");
