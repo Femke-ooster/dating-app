9cfa50565f0013ac5ab500e0366054ab3f266725
Mona Lisa <femkedrawings@gmail.com>
2020-03-24 14:44:17 +0100

    Notificaties blijven staan
    EINDELIJK

--- feature/index.js
+++ feature/index.js
@@ -1,16 +1,18 @@
-const express = require('express');
 var slug = require('slug');
 var bodyParser = require('body-parser');
 var mongo = require('mongodb');
-/*env beschikbaarvoor node.js*/
-require('dotenv').config();
+const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = 8000;
+/*env beschikbaarvoor node.js*/
+require('dotenv').config();
 
 let db;
 const MongoClient = require('mongodb').MongoClient;
 const uri = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@cluster0-lrmlb.azure.mongodb.net/test?retryWrites=true&w=majority";
 /*const uri = "mongodb+srv://femkeooster:<password>@cluster0-lrmlb.azure.mongodb.net/test?retryWrites=true&w=majority";*/
+
 
 /*verbind met mongodb*/
 MongoClient.connect(uri, function (err, client) {
@@ -26,61 +28,72 @@ MongoClient.connect(uri, function (err, client) {
 /*geef toegang tot statische map*/
 app.use("/static", express.static(__dirname + "/static")); /**/
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+	resave: false,
+	saveUninitialized: true,
+	secret: process.env.SESSION_SECRET
+}));
 /*Zeg tegen Express dat we de ejs template engine gebruiken*/
 app.set("view engine", "ejs");
 /*Zeg tegenb Express dat onze HTMl 'views' in een map views te vinden zijn*/
 app.set("views", "views");
 
-
+//GET
 
 app.get("/", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
-  res.redirect("/home",);
+  res.redirect("/home");
 });
 
 app.get("/home", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
-  res.render("index.ejs", {title: "Matchie"});
+  res.render("index.ejs", req.session.note);
 });
 
 app.get("/boost", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
-  res.render("boost.ejs", {title: "Boost"});
+  res.render("boost.ejs", req.session.note);
 });
 
 app.get("/likes", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
-  res.render("likes.ejs", {title: "Likes"});
+  res.render("likes.ejs", req.session.note);
 });
 
 app.get("/messages", function(req, res){
   /*method .render beschikbaar gesteld door ejs op het response object*/
-  res.render("messages.ejs", {title: "Messages"});
+  res.render("messages.ejs", req.session.note);
 });
 
+app.get("/input", inputData);
 
+function inputData(req, res){
+  db.collection('notification').insertOne({
+    boost: req.body.inBoost,                    // opgeslagen object
+    likes: req.body.inLikes,
+    messages: req.body.inMessages
+  })
+  function error(err, result){                  // callback
+      if(err) throw err;
+      res.status(200).send('data inserted')
+  }
+  console.log(req.session.note);
+  res.render("test/input.ejs", req.session.note);  // render input.ejs pagina
+};
 
-app.get("/input", function(req, res){
-  res.render("test/input.ejs", req.body.note);
-});
+//POST
 
-app.post('/input', addBoost)
+app.post('/inputNotifications', addBoost)
 
 function addBoost(req, res){
 
-    req.body.note = {
+    req.session.note = {
       boost: req.body.inBoost,
       likes: req.body.inLikes,
       messages: req.body.inMessages
   };
-  console.log(req.body.note); //Laat in de terminal de ingevulde gegevens zien.
-  res.render("index.ejs", req.body.note);
+  console.log(req.session.note); //Laat in de terminal de gegevens zien.
+  res.redirect('home')
 };
 
-
 app.listen(port, () => console.log("Running my NodeJS server at" + port));
-
-/*method .render beschikbaar gesteld door ejs op het response object*/
-//   db.collection('note').findOne((err, notification) => {
-//   console.log(notification)
-// })
